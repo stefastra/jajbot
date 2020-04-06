f28c70616181c44d07ae9f7f71112adfa2151996
Stefanos <stefastra@gmail.com>
2020-04-06 06:26:46 +0300

    added play policing

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-
-let rawdata = fs.readFileSync('token.json');
-let token = JSON.parse(rawdata);
-console.log(token);
-
 const client = new Discord.Client();
 const prefix = '~';
+const token = JSON.parse(fs.readFileSync('token.json'))
 var usePrefix = false;
 
-client.on('ready', ()=>{
-    console.log('jaj is online, id: ' + client.user.tag);
+client.login(token);
 
-    client.user.setPresence({
-        status: "online",
-        game: {
-            name: "mandarine sandwich",
-            type: "im eating"
-        }
-    })
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+client.on('ready', ()=>{
+    console.log('jaj is online, id: ' + client.user.tag + '. token is valid.');
+
+    /*let tempChannel = message.guild.channels.find('le-test')
+    tempChannel.send('jupnisa, kalimera xalarwse')*/
 });
 
-client.on('message', message=>{
+var flag = true;
+client.on('message', async message=>{
     let args = message.content.substring(prefix.length).split(" ");
+    
+    if(message.content[0] =='-' && message.content[1]=='p' && message.channel!='487383328161267714'){
+        message.delete();
+        if(flag){
+            message.reply(':man_police_officer: haha bruh den epitrepete auto pou kaneis, mono sto <#487383328161267714>')
+            .then(msg =>{
+                msg.delete({timeout:3000});
+            })
+            flag=false
+        }
+        else{
+            message.reply(':oncoming_police_car: iou iou iou, astynomos jaj edw, stamata, mono sto <#487383328161267714>')
+            .then(msg =>{
+                msg.delete({timeout:3000});
+            })
+            flag=true
+        }
+    }
 
-    if(!usePrefix){
-        switch(args[0]){
+        switch(message.content){
             case 'geia':
-                message.reply('geia');
+                message.reply('geia ' + message)
             break;
-            case 'jaj':
+            case 'jaj?':
                 message.channel.send('geia xalarwse')
             break;
             case 'info':
                 message.channel.send('haha bruh: https://github.com/stefastra/jajbot')
             break;
             case 'mpes':
-                if(!message.member.voice){
+                if(false){
                     message.channel.send('pou re mlk')
                 }
                 else{
                     if(!message.guild.voice)
-                        client.joinVoiceChannel()         
+                        client.joinVoiceChannel();
                 }
-        }
-    }
-    if(usePrefix){
-        
-    }
-})
-
-client.login(token);
+            break;
+            case 'mu lipis':
+                message.channel.send(':point_right: :point_left:')
+            break;
+            case 'jaj':
+                message.channel.send(':point_left: :point_right:')
+            break;
+            case 'ti trws?':
+                message.channel.send('🍊🥪')
+            break;
+        };
+});
