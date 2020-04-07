28d1d49e56830cf580100eb7a0eb4d17ffba0d9f
Stefanos <stefastra@gmail.com>
2020-04-07 20:07:58 +0300

    minor tweaks

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('message', async message=>{
     let args = message.content.substring(prefix.length).split(" ");
     let ytlink = message.content.substring(0,32);
     let ytlinkalt = message.content.substring(0,17);
-    let mCon = message.content;
+    let mCon = message.content.toLowerCase();
 
     //1: bot-commands, 2: music, music exception is needed to avoid duplicate chat moderation rules
     if(((mCon[0]=='-' && mCon[1]=='p') || (mCon[0]=='-' && mCon[1]=='s') || (mCon[0]=='-' && mCon[1]=='r')) && message.channel!='487383328161267714' && message.channel!='487381111744233473'){
@@ -54,10 +54,11 @@ client.on('message', async message=>{
             })
             flag=true
         }
+        console.log('misplaced bot command eliminated, in ' + message.channel.name)
     }
     
     if(ytlink!='https://www.youtube.com/watch?v=' && ytlinkalt!='https://youtu.be/' && message.channel=='487381111744233473' && message.author.id!='693966110328094730'){
-        console.log('non-youtube link spotted');
+        console.log('non-youtube link spotted in music channel');
         message.delete();
         if(message.author.id!='234395307759108106')
             message.reply(':man_police_officer: bruh den epitrepete auto pou kaneis, mono youtube links')
@@ -66,42 +67,57 @@ client.on('message', async message=>{
             });
     }
 
-
-    if(mCon=='jaj steile'){
-        while(true){
-            console.log('ACTIVATING DOOM...')
-            message.channel.send(prntUrlBase + prntUrlVarStr + prntUrlVarNum.toString());
-            prntUrlVarNum--;
-            await sleep(3000);
-            console.log('SWEEP DONE')
+    if(mCon=='jaj svise'){
+        if(message.member.roles.cache.has('340521348260429824')){
+            console.log('o jaj esvise 10 minimata sto ', message.channel.name);
+            message.channel.bulkDelete(10);
+        }
+        else{
+            message.reply('den exeis adeia gia auto bro..');
         }
     }
 
+    if(mCon=='jaj steile' && message.channel.id=='696857160318976021'){
+        while(true){
+            console.log('Sending pic...');
+            message.channel.send(prntUrlBase + prntUrlVarStr + prntUrlVarNum.toString());
+            prntUrlVarNum--;
+            await sleep(3000);
+            console.log('Sent!');
+        }
+    }
+
+//generic responses
         switch(mCon){
             case 'geia':
-                message.reply('geia ' + message)
+                message.reply('geia');
+                message.channel.send('<:peepoSmile:629010685095051264>');
+            break;
+            case 'jaj':
+                message.channel.send('ela')
             break;
             case 'jaj?':
                 if(message.author.id=='396653783804936202'){
                     if(flagK){
-                        message.channel.send('malaka kargioli poustanio')
+                        message.channel.send('malaka kargioli poustanio');
                         flagK=false;
                     }
                     else{
-                        message.channel.send('voulose to malaka apotiximene paizeis rocket league olh mera kai thn exeis dei kapoios, apla vgale ton skasmo, malaka')
+                        message.channel.send('voulose to malaka apotiximene paizeis rocket league olh mera kai thn exeis dei kapoios, apla vgale ton skasmo, malaka');
                         flagK=true;
                     }
                 }else{
-                    message.channel.send('geia xalarwse')
+                    message.channel.send('geia xalarwse');
 
                 }
             break;
             case 'info':
-                message.channel.send('haha bruh: https://github.com/stefastra/jajbot')
+                message.channel.send('haha bruh: https://github.com/stefastra/jajbot');
             break;
             case 'mpes':
-                if(false){
-                    message.channel.send('pou re mlk')
+                if(true){
+                    message.channel.send('pou re mlk');
+                    message.channel.send('<:peepoNpc:536307730361745418>');
                 }
                 else{
                     if(!message.guild.voice)
@@ -109,13 +125,13 @@ client.on('message', async message=>{
                 }
             break;
             case 'mu lipis':
-                message.channel.send(':point_right: :point_left:')
+                message.channel.send(':point_right: :point_left:');
             break;
             case 'jaj':
-                message.channel.send(':point_left: :point_right:')
+                message.channel.send(':point_left: :point_right:');
             break;
             case 'ti trws?':
-                message.channel.send('🍊🥪')
+                message.channel.send('🍊🥪');
             break;
         };
     })
